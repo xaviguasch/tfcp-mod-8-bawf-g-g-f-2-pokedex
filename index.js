110918c9b7a1a8c47b38c2a668fa8d18b9edad78
xaviguasch <xaviguasch@gmail.com>
2020-09-02 12:07:55 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+    Pokedex
+    
+    Write an async function 
+        that uses fetch() to fetch all 
+        Pokemon from pokemon.json
+    
+    Display all the Pokemon
+        ID, English Name, 
+        Type(s), 
+        Stats: HP/Attack/Defense/Speed
+*/
