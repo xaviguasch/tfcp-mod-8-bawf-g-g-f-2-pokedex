cbea92ed2416f6e558bace0ac2983aaad4f03390
xaviguasch <xaviguasch@gmail.com>
2020-09-02 19:18:15 +0200

    solved first part of the challenge

--- index.js
+++ index.js
@@ -10,3 +10,33 @@
         Type(s), 
         Stats: HP/Attack/Defense/Speed
 */
+
+async function getAllPokemon() {
+  let response = await fetch('pokemon.json')
+  let data = await response.json()
+  return data
+}
+
+function getPokemonHtml(aPokemon) {
+  return `
+    <div class="a-pokemon">
+      <div class="a-pokemon-id">${aPokemon.id}</div>
+      
+      <div class="a-pokemon-name">${aPokemon.name.english}</div>
+      <div class="a-pokemon-type">${aPokemon.type.join(' / ')}</div>
+      
+      <div class="a-pokemon-stat">HP: ${aPokemon.base.HP}</div>
+      <div class="a-pokemon-stat">Attack: ${aPokemon.base.Attack}</div>
+      <div class="a-pokemon-stat">Defense: ${aPokemon.base.Defense}</div>
+      <div class="a-pokemon-stat">Speed: ${aPokemon.base.Speed}</div>
+    </div>
+    `
+}
+
+getAllPokemon().then((allPokemon) => {
+  document.body.innerHTML = `
+    <div class="my-pokedex>
+      ${allPokemon.map((aPokemon) => getPokemonHtml(aPokemon)).join('')}
+    </div>
+  `
+})
